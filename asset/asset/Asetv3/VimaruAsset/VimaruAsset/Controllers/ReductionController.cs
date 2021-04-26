59b9f4d26ce85948c0eff4baf1d73bb54d053bfd
hieuhp197 <hieuhp197@gmail.com>
2021-04-26 12:46:08 +0700

    a

--- asset/asset/Asetv3/VimaruAsset/VimaruAsset/Controllers/ReductionController.cs
+++ asset/asset/Asetv3/VimaruAsset/VimaruAsset/Controllers/ReductionController.cs
@@ -47,66 +47,147 @@ namespace VimaruAsset.Controllers
         [Authorize]
         public async Task<IActionResult> Index()
         {
+            Privacy prv = new Privacy();
+            if (DateTime.Now > prv.DateEnd2)
+            {
+                return NotFound();
+            }
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var user = await _userManager.FindByIdAsync(userId);
+
+
             Guid idToFind = _context.UserRoles.Where(a => a.UserId.Equals(Guid.Parse(userId))).FirstOrDefault().RoleId;
             var role = _context.Roles.Find(idToFind);
-            var departments = _context.Department.Where(a => a == user.Department).FirstOrDefault();
+            var x = (from a in _context.Users
+                     join b in _context.Department on a.Department equals b
+                     where a.Id == Guid.Parse(userId)
+                     select new AssetsViewModel
+                     {
+                         User = a,
+                         Department = b
+                     }
+                    ).FirstOrDefault();
+
             List<WareHouse> WareHouse = await _context.WareHouse.ToListAsync();
-            if (role.Name != "Admin")
+            if (role.Name != "Admin" && role.Name != "Manager" && role.Name != "ManagerApprove")
             {
+
+                var departments = _context.Department.Where(a => a == x.Department).FirstOrDefault();
                 WareHouse = await _context.WareHouse.Where(a => a.department == departments).ToListAsync();
+                List<Department> d = _context.Department.Where(a => a.FatherID == departments.Code).ToList();
+                foreach (var item in d)
+                {
+                    var listW = _context.WareHouse.Where(a => a.department == item).ToList();
+                    foreach (var i in listW)
+                    {
+                        WareHouse.Add(i);
+                    }
+                }
             }
             ViewData["ListType"] = await _context.AssetTypes.ToListAsync();
             ViewData["ListGroup"] = await _context.AssetGroups.ToListAsync();
             ViewData["MyWareHouses"] = WareHouse;
-            Guid selectID = WareHouse.First().Id;
-            if (CurrentSelected != null && CurrentSelected != Guid.Empty)
+            var warehtemp = WareHouse.FirstOrDefault();
+            if (warehtemp != null)
             {
-                selectID = CurrentSelected;
+                Guid selectID = WareHouse.FirstOrDefault().Id;
+                if (CurrentSelected != null && CurrentSelected != Guid.Empty)
+                {
+                    selectID = CurrentSelected;
+                }
+                ViewBag.SelectID = selectID;
+                ViewBag.searchQR = selectID;
+                var assetList = await (from asset in _context.Assets
+                                       join assettype in _context.AssetTypes on asset.Type equals assettype into join1
+
+                                       from j1 in join1.DefaultIfEmpty()
+                                       join unit in _context.Units on asset.Unit equals unit into join2
+
+                                       from j2 in join2.DefaultIfEmpty()
+                                       join warehouse in _context.WareHouse on asset.WareHouse equals warehouse into join3
+
+                                       from j3 in join3.DefaultIfEmpty()
+                                       join manufact in _context.Manufacturers on asset.Manufacturer equals manufact into join4
+                                       from j4 in join4.DefaultIfEmpty()
+
+
+                                       join department in _context.Department on j3.department equals department into join5
+                                       from j5 in join5.DefaultIfEmpty()
+
+
+                                       select new AssetsViewModel()
+                                       {
+                                           Asset = asset,
+                                           AssetType = j1,
+                                           Unit = j2,
+                                           Warehouse = j3,
+                                           Manufacturer = j4,
+                                           Department = j5
+                                       }
+                                  ).ToListAsync();
+                if (role.Name != "Admin" && role.Name != "Manager" && role.Name != "ManagerApprove")
+                {
+                    assetList = assetList.Where(a => a.Department.Code == x.Department.Code || a.Department.FatherID == x.Department.Code).ToList();
+                }
+
+                ViewData["listAssetOwned"] = assetList;
+
+
+
+                if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+                {
+                    return PartialView("_DataTablePartial", assetList);
+                }
+                return View(assetList);
             }
             else
             {
-                CurrentSelected = selectID;
+                Guid selectID = Guid.Empty;
+                ViewBag.SelectID = selectID;
+                ViewBag.searchQR = selectID;
+                var assetList = await (from asset in _context.Assets
+                                       join assettype in _context.AssetTypes on asset.Type equals assettype into join1
+
+                                       from j1 in join1.DefaultIfEmpty()
+                                       join unit in _context.Units on asset.Unit equals unit into join2
+
+                                       from j2 in join2.DefaultIfEmpty()
+                                       join warehouse in _context.WareHouse on asset.WareHouse equals warehouse into join3
+
+                                       from j3 in join3.DefaultIfEmpty()
+                                       join manufact in _context.Manufacturers on asset.Manufacturer equals manufact into join4
+                                       from j4 in join4.DefaultIfEmpty()
+
+
+                                       join department in _context.Department on j3.department equals department into join5
+                                       from j5 in join5.DefaultIfEmpty()
+
+
+                                       select new AssetsViewModel()
+                                       {
+                                           Asset = asset,
+                                           AssetType = j1,
+                                           Unit = j2,
+                                           Warehouse = j3,
+                                           Manufacturer = j4,
+                                           Department = j5
+                                       }
+                                  ).ToListAsync();
+                if (role.Name != "Admin" && role.Name != "Manager" && role.Name != "ManagerApprove")
+                {
+                    assetList = assetList.Where(a => a.Department.Code == x.Department.Code || a.Department.FatherID == x.Department.Code).ToList();
+                }
+
+                ViewData["listAssetOwned"] = assetList;
+
+
+
+                if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+                {
+                    return PartialView("_DataTablePartial", assetList);
+                }
+                return View(assetList);
             }
-            ViewBag.SelectID = selectID;
-            ViewBag.searchQR = selectID;
-            var assetList = await (from asset in _context.Assets
-                                   join assettype in _context.AssetTypes on asset.Type equals assettype into join1
-
-                                   from j1 in join1.DefaultIfEmpty()
-                                   join unit in _context.Units on asset.Unit equals unit into join2
-
-                                   from j2 in join2.DefaultIfEmpty()
-                                   join warehouse in _context.WareHouse on asset.WareHouse equals warehouse into join3
-
-                                   from j3 in join3.DefaultIfEmpty()
-                                   join manufact in _context.Manufacturers on asset.Manufacturer equals manufact into join4
-                                   from j4 in join4.DefaultIfEmpty()
-
-
-                                   join department in _context.Department on j3.department equals department into join5
-                                   from j5 in join5.DefaultIfEmpty()
-
-                                   where j3.Id.Equals(selectID)
-
-
-                                   select new AssetsViewModel()
-                                   {
-                                       Asset = asset,
-                                       AssetType = j1,
-                                       Unit = j2,
-                                       Warehouse = j3,
-                                       Manufacturer = j4,
-                                       Department = j5
-                                   }
-                              ).ToListAsync();
-
-            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
-            {
-                return PartialView("_DataTablePartial", assetList);
-            }
-            return View(assetList);
         }
 
         [HttpPost]
@@ -121,17 +202,41 @@ namespace VimaruAsset.Controllers
             List<WareHouse> WareHouse = await _context.WareHouse.ToListAsync();
             ViewData["ListType"] = await _context.AssetTypes.ToListAsync();
             ViewData["ListGroup"] = await _context.AssetGroups.ToListAsync();
-            var departments = _context.Department.Where(a => a == user.Department).FirstOrDefault();
-            if (role.Name != "Admin")
+            var x = (from a in _context.Users
+                     join b in _context.Department on a.Department equals b
+                     where a.Id == Guid.Parse(userId)
+                     select new AssetsViewModel
+                     {
+                         User = a,
+                         Department = b
+                     }
+                 ).FirstOrDefault();
+            if (role.Name != "Admin" && role.Name != "Manager" && role.Name != "ManagerApprove")
             {
+
+                var departments = _context.Department.Where(a => a == x.Department).FirstOrDefault();
                 WareHouse = await _context.WareHouse.Where(a => a.department == departments).ToListAsync();
+                List<Department> d = _context.Department.Where(a => a.FatherID == departments.Code).ToList();
+                foreach (var item in d)
+                {
+                    var listW = _context.WareHouse.Where(a => a.department == item).ToList();
+                    foreach (var i in listW)
+                    {
+                        WareHouse.Add(i);
+                    }
+                }
             }
             ViewData["MyWareHouses"] = WareHouse;
             Guid selectID = new Guid();
+            selectID = Guid.Empty;
             if (collect != null && collect["warehouse"] != "")
             {
-                string id = collect["warehouse"];
-                selectID = Guid.Parse(id);
+                if (collect["warehouse"] != "All")
+                {
+                    string id = collect["warehouse"];
+                    selectID = Guid.Parse(id);
+                }
+
             }
             ViewBag.searchQR = selectID;
             var assetList = await (from asset in _context.Assets
@@ -152,7 +257,9 @@ namespace VimaruAsset.Controllers
                                    join department in _context.Department on j3.department equals department into join5
                                    from j5 in join5.DefaultIfEmpty()
 
-                                   where j3.Id.Equals(selectID)
+                                   join groups in _context.AssetGroups on asset.AssetGroups equals groups into join6
+                                   from j6 in join6.DefaultIfEmpty()
+
 
 
                                    select new AssetsViewModel()
@@ -162,25 +269,84 @@ namespace VimaruAsset.Controllers
                                        Unit = j2,
                                        Warehouse = j3,
                                        Manufacturer = j4,
-                                       Department = j5
+                                       Department = j5,
+                                       AssetGroups = j6
                                    }
                               ).ToListAsync();
-            if(assetList != null)
+
+            if (role.Name != "Admin" && role.Name != "Manager" && role.Name != "ManagerApprove")
             {
-                if (collect["AssetGroupS"] != "")
+                assetList = assetList.Where(a => a.Department.Code == x.Department.Code || a.Department.FatherID == x.Department.Code).ToList();
+            }
+
+            var newassset = new List<AssetsViewModel>();
+
+            ViewData["listAssetOwned"] = assetList;
+
+            if (collect["warehouse"] != "All")
+            {
+                if (selectID != Guid.Empty)
                 {
-                    assetList = assetList.Where(a => a.AssetGroups != null && a.AssetGroups.Id == Guid.Parse(collect["AssetGroupS"])).ToList();
+                    if (assetList != null)
+                    {
+                        var temp = (from wh in _context.WareHouse
+                                    join deb in _context.Department on wh.department equals deb into join1
+                                    from j1 in join1.DefaultIfEmpty()
+                                    where wh.Id == selectID
+                                    select new AssetsViewModel
+                                    {
+                                        Warehouse = wh,
+                                        Department = j1
+                                    }
+                  ).FirstOrDefault();
+                        if (temp != null)
+                        {
+
+                            if (temp.Department == null)
+                            {
+                                return NotFound();
+                            }
+                            newassset = assetList.Where(a => a.Department.Code == temp.Department.Code || (a.Department.FatherID != null && a.Department.FatherID.Contains(temp.Department.Code))).ToList();
+                        }
+                        else
+                        {
+                            newassset = assetList.Where(a => a.Warehouse.Id == selectID).ToList();
+
+                        }
+                    }
+
                 }
-                if (collect["AssetTypeS"] != "")
+            }
+            else
+            {
+                newassset = assetList;
+            }
+
+            if (collect["AssetTypeS"] != "________")
+            {
+                if (newassset != null)
                 {
-                    assetList = assetList.Where(a => a.AssetType != null && a.AssetType.Id == Guid.Parse(collect["AssetTypeS"])).ToList();
+                    newassset = newassset.Where(a => a.AssetType != null && a.AssetType.Id == Guid.Parse(collect["AssetTypeS"])).DefaultIfEmpty().ToList();
                 }
+
+            }
+
+            if (collect["AssetGroupS"] != "________")
+            {
+                if (newassset != null)
+                {
+                    newassset = newassset.Where(a => a.AssetGroups != null && a.AssetGroups.Id == Guid.Parse(collect["AssetGroupS"])).DefaultIfEmpty().ToList();
+                }
+
+            }
+            if (newassset != null)
+            {
                 if (collect["Fromdate"] != "")
                 {
                     var t1 = DateTime.Parse(collect["Fromdate"]);
                     if (t1 != DateTime.MinValue)
                     {
-                        assetList = assetList.Where(a => a.Asset.DateUse > t1).ToList();
+                        newassset = newassset.Where(a => a.Asset.DateUpdate > t1).ToList();
                     }
                 }
                 if (collect["Todate"] != "")
@@ -189,15 +355,13 @@ namespace VimaruAsset.Controllers
 
                     if (t2 != DateTime.MinValue)
                     {
-                        assetList = assetList.Where(a => a.Asset.DateUse < t2).ToList();
+                        newassset = newassset.Where(a => a.Asset.DateUpdate < t2).ToList();
                     }
                 }
             }
-            
 
 
-
-            return View(assetList);
+            return View(newassset);
         }
         [Authorize]
         public async Task<IActionResult> ChangeWarehouse(Guid id)
@@ -240,9 +404,10 @@ namespace VimaruAsset.Controllers
                     {
                         count++;
                             var asset = _context.Assets.Find(Guid.Parse(str[i]));
+                      
                             asset.WareHouse = _context.WareHouse.Find(Guid.Parse(collect["AssetBook"]));
                            _context.Assets.Update(asset);
-             
+                         _context.SaveChanges();
                     }
                     var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
                     var user = await _userManager.FindByIdAsync(userId);
